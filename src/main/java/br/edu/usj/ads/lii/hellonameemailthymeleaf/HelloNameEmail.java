d794dc98fbb1758c7f724b6544b4d84c538d7378
Matheus Alves <matttalves@gmail.com>
2020-10-24 14:29:56 -0300

    Trocando o método POST por GET

--- src/main/java/br/edu/usj/ads/lii/hellonameemailthymeleaf/HelloNameEmail.java
+++ src/main/java/br/edu/usj/ads/lii/hellonameemailthymeleaf/HelloNameEmail.java
@@ -1,15 +1,16 @@
 package br.edu.usj.ads.lii.hellonameemailthymeleaf;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @Controller
 public class HelloNameEmail {
     
-    @PostMapping(value="olaPessoa")
+    @GetMapping(value="olaPessoa")
     public ModelAndView postHelloNameEmail(@RequestParam String nome, @RequestParam String email) {
         
         ModelAndView modelAndView = new ModelAndView("name-email");
